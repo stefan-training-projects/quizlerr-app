7daae434e2bcd61ee240259120b2049b2ec1ecca
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-03-15 06:43:13 +0200

    get_next_question

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
@@ -16,7 +19,9 @@ class QuizInterface:
                                 150,
                                 125,
                                 text="Text",font=("Arial",20,"italic"),
-                                fill=THEME_COLOR)
+                                fill=THEME_COLOR,
+                                width=280
+                            )
         self.canvas.grid(row=1,column=0,columnspan=2,pady=20)
 
         #labels
@@ -31,4 +36,10 @@ class QuizInterface:
         self.button_false= Button(image=false_image,highlightthickness=0,bg=THEME_COLOR)
         self.button_false.grid(row=2,column=1,pady=20)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text = q_text)
